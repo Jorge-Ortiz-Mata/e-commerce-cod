e77aa747eca3661ce882500285d7f96104ff8bb2
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2024-02-27 20:06:44 -0600

    feat: add search by name form

--- src/components/common/ProductList/index.jsx
+++ src/components/common/ProductList/index.jsx
@@ -1,16 +1,65 @@
+import { useEffect, useState } from "react";
 import { useAtom } from "jotai";
 import { productsAtom } from "../../../store";
 import Product from "../Product";
 
 const ProductList = () => {
   const [products] = useAtom(productsAtom);
+  const [productsFiltered, setProductsFiltered] = useState([]);
+  const [inputValue, setInputValue] = useState("")
+
+  useEffect(() => {
+    const productsFiltered = products.filter(product => {
+      return product.name.includes(inputValue);
+    });
+
+    if(productsFiltered.length > 0) {
+      setProductsFiltered(productsFiltered);
+    } 
+
+  }, [inputValue]);
+
+  const sortByPrice = () => {
+    // products.sort((a, b) => {
+    //   return parseInt(a.price) - parseInt(b.price);
+    // });
+  }
+
+  const handleOnChange = (e) => {
+    const { value } = e.target;
+    setInputValue(value);
+  }
 
   return(
     <div className="flex flex-col w-full h-full gap-5">
-      <h3 className="font-semibold text-lg text-cyan-700">Your Products</h3>
+      <div className="flex items-center justify-between">
+        <h3 className="font-semibold text-lg text-cyan-700">Your Products</h3>
+        
+        <form>
+          <input 
+            type="text"
+            id="search"
+            name="search"
+            className="border py-1 px-2 text-sm text-gray-600 rounded"
+            value={inputValue}
+            placeholder="search product"
+            onChange={handleOnChange}
+          />
+        </form>
+      </div>
+
+      <div className="flex items-center justify-end">
+        <button 
+          onClick={sortByPrice}
+          className="text-sm text-white bg-orange-500 rounded px-3 py-1"
+        >
+          Sort by price
+        </button>
+      </div>
+
       <div className="flex flex-col gap-3">
         {
-          products.map(product => {
+          productsFiltered.map(product => {
             return <Product key={product.name} product={product} />
           })
         }
--- src/components/common/forms/NewProduct/index.jsx
+++ src/components/common/forms/NewProduct/index.jsx
@@ -35,6 +35,7 @@ const NewProductForm = () => {
 
   return(
     <form className="flex flex-col items-center justify-center border w-2/3 gap-5 p-5 rounded shadow bg-white">
+      <h3 className="text-center text-cyan-700 font-semibold">New Product</h3>
       <div className="flex flex-col items-start w-full gap-1">
         <CustomLabel 
           title="Name:"
